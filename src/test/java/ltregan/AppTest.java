4a00306e783b911595063774d8b12c73d5196225
ltregan <l.tregan@gmail.com>
2015-09-29 18:00:42 +0200

    test ready

--- src/test/java/ltregan/AppTest.java
+++ src/test/java/ltregan/AppTest.java
@@ -1,38 +1,40 @@
 package ltregan;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Created by loic on 9/27/2015.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testApp() throws Exception{
+
+        String json = "{  id: \"33\", name: \"loic\"}";
+        NodeBuilder builder = new NodeBuilder();
+        builder.settings().put("path.home", "c:/temp"+System.currentTimeMillis());
+        //  builder.settings().put("index.store.type","memory");
+        Node node = builder.local(true).node();
+        Client client = node.client();
+
+        IndexResponse r = client
+                .prepareIndex("lookbooks", "lookbook")
+                .setId( "33" )
+                .setSource( json )
+                .execute()
+                .actionGet();
+
+        GetResponse response = client.prepareGet("lookbooks", "lookbook", "33")
+                .execute()
+                .actionGet();
+        System.out.println(response.getSource());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
